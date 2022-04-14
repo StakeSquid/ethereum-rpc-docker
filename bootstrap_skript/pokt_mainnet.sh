96c90dee4b581d40491ae77b2593a1206c1b1204
cventastic <sven@sven-holter.de>
2022-04-14 18:40:18 +0200

    only touch setupdone for pokt bootstrap if tar command succeeds

--- bootstrap_skript/pokt_mainnet.sh
+++ bootstrap_skript/pokt_mainnet.sh
@@ -3,7 +3,5 @@ if [ ! -f /home/app/.pocket/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
   echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xv -C /home/app/.pocket/data/"
-  wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/
-  touch /home/app/.pocket/setupdone
+  wget -q -O - $POCKET_SNAPSHOT | tar -xv -C /home/app/.pocket/data/ && touch /home/app/.pocket/setupdone
 fi
-
