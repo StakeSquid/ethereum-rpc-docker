515be431812ff79a0bc9d3ff4a8bf53b6ac9b2a8
cventastic <sven@sven-holter.de>
2022-04-26 10:57:05 +0200

    add owner/group to bootstrap.sh

--- pokt/bootstrap.sh
+++ pokt/bootstrap.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /home/app/.pocket/data/setupdone ]
 then
-  mkdir -p /home/app/.pocket/data
+  mkdir -o 1005 -g 1001 -p /home/app/.pocket/data
   echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xzv -C /home/app/.pocket/data/"
   wget -q -O - $POCKET_SNAPSHOT | tar -xzv -C /home/app/.pocket/data/
   touch /home/app/.pocket/data/setupdone
