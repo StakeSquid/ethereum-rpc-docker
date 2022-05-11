97eb058ecfd33553ce93e7881837b31183679a25
cventastic <sven@sven-holter.de>
2022-05-11 15:55:53 +0200

    fix tar syntax for new snapshot format

--- pokt/bootstrap.sh
+++ pokt/bootstrap.sh
@@ -3,8 +3,8 @@
 if [ ! -f /home/app/.pocket/data/setupdone ]
 then
   mkdir -p /home/app/.pocket/data
-  echo "wget -q -O - '$POCKET_SNAPSHOT' | tar xfvz -C /home/app/.pocket/data/"
-  wget -q -O - $POCKET_SNAPSHOT | tar xfvz -C /home/app/.pocket/data/
+  echo "wget -q -O - '$POCKET_SNAPSHOT' | tar -xvz -C /home/app/.pocket/data/"
+  wget -q -O - $POCKET_SNAPSHOT | tar -xvz -C /home/app/.pocket/data/
   touch /home/app/.pocket/data/setupdone
 fi
 
