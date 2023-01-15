751bb532e50828d2f63aff7a50ef6f1cd85e4efb
Sebastian <379651+czarly@users.noreply.github.com>
2023-01-15 09:31:01 +0100

    init the arb classic node with a snapshot

--- arbitrum/classic-entrypoint.sh
+++ arbitrum/classic-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "now we can download the db.tar file"
+if [ -f /root/.arbitrum/mainnet/INITIALIZED ]; then
+    echo "datadir is already initialized"
+else
+    echo "lemme download the database quickly"
+    rm -rf /root/.arbitrum/mainnet/db
+    curl https://snapshot.arbitrum.io/mainnet/db.tar | tar -xv -C /root/.arbitrum/mainnet/ && touch /root/.arbitrum/mainnet/INITIALIZED    
+fi
+
+echo "LFG!!!"
 
 /home/user/go/bin/arb-node $@
