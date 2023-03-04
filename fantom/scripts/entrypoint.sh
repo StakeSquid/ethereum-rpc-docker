95cfb4e62c0522b6e39c811afe96d307bac78228
Sebastian <379651+czarly@users.noreply.github.com>
2023-03-04 13:17:04 +0100

    this is fantom archive

--- fantom/scripts/entrypoint.sh
+++ fantom/scripts/entrypoint.sh
@@ -16,23 +16,27 @@ if [ ! -f "$FANTOM_HOME/mainnet.g" ];
 then
     cd $FANTOM_HOME
     echo "downloading launch genesis file"
-    wget --quiet https://opera.fantom.network/mainnet.g
+    wget --quiet https://download.fantom.network/mainnet-5577-full-mpt.g
 fi
 
 opera \
-    --genesis=/datadir/mainnet.g \
+    --genesis=/datadir/mainnet-5577-full-mpt.g \
     --port=5050 \
     --maxpeers=200 \
     --datadir=/datadir \
     --http \
     --http.addr=0.0.0.0 \
     --http.port=18545 \
-    --http.api=ftm,eth,debug,admin,web3,personal,net,txpool,sfc \
+#    --http.api=ftm,eth,debug,admin,web3,personal,net,txpool,sfc \
     --http.corsdomain="*" \
     --http.vhosts="*" \
     --ws \
     --ws.addr=0.0.0.0 \
     --ws.port=18546 \
-    --ws.api=ftm,eth,debug,admin,web3,personal,net,txpool,sfc \
+#    --ws.api=ftm,eth,debug,admin,web3,personal,net,txpool,sfc \
     --ws.origins="*" \
-    --nousb
+    --nousb \
+#    --db.migration.mode reformat \
+    --db.preset pbl-1 \
+    --tracenode \
+    --http.api=eth,web3,net,ftm,trace
