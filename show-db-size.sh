709fbb3aeaa10cd324f2ad7e3bd40b7527c3b5ae
Sebastian <379651+czarly@users.noreply.github.com>
2023-08-25 06:32:12 +0200

    better output

--- show-db-size.sh
+++ show-db-size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find /var/lib/docker/volumes/ -maxdepth 1 -type d -name 'rpc_*' -exec du -sh {} \; | sort -rh
+df -h / | awk 'NR==2 {print "Total: " $2, "Free: " $4}'
+
+find /var/lib/docker/volumes -maxdepth 1 -type d -name 'rpc_*' -exec du -sh {} \; | sort -rh | awk '{cmd="basename "$2; cmd | getline dir; close(cmd); sub(/^rpc_/, "", dir); print $1, dir}'
