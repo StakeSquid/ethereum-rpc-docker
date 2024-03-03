458eb8081f1edd4f1edd49ad2dee10870af44d4b
Sebastian <379651+czarly@users.noreply.github.com>
2024-03-03 07:59:02 +0100

    fix

--- linea/geth/scripts/entrypoint.sh
+++ linea/geth/scripts/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 GETH_HOME=/root/.ethereum
 
-if [ ! -f "${ERIGON_HOME}/bootstrapped.new" ];
+if [ ! -f "${GETH_HOME}/bootstrapped.new" ];
 then
   echo "write the custom genesis block"
   mkdir -p ${GETH_HOME:-/root/.ethereum}
