0d72a7d0a7d6339d1b551a351cea9cd02215f92a
Sebastian <379651+czarly@users.noreply.github.com>
2024-03-19 06:11:54 +0100

    stats

---
+++ show-status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEPATH=/root/rpc
+source $BASEPATH/.env
+
+IFS=':' read -ra parts <<< $COMPOSE_FILE
+
+blacklist=("drpc.yml" "base.yml" "rpc.yml" "monitoring.yml")
+
+for part in "${parts[@]}"; do
+    include=true
+    for word in "${blacklist[@]}"; do
+	if echo "$part" | grep -qE "$word"; then
+	    #echo "The path $path contains a blacklisted word: $word"
+	    include=false
+	fi
+    done
+    
+    if $include; then
+	result=$(./sync-status.sh "${part%.yml}")
+	echo "$part: $result"
+    fi
+done
+	
+	
+	
