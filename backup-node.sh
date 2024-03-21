d4449d445ed8c5c95e267df04d8332231a635aea
Sebastian <379651+czarly@users.noreply.github.com>
2024-03-21 05:35:23 +0100

    add teh information

--- backup-node.sh
+++ backup-node.sh
@@ -13,7 +13,8 @@ for key in $keys; do
     echo "Executing command with key: /var/lib/docker/volumes/rpc_$key/_data"
 
     source_folder="/var/lib/docker/volumes/rpc_$key/_data"
-    target_file="/backup/rpc_$key-$(date +'%Y-%m-%d-%H-%M-%S').tar.zst"
+    folder_size=$(du -sh "$source_folder" | awk '{print $1}')
+    target_file="/backup/rpc_$key-$(date +'%Y-%m-%d-%H-%M-%S')-$folder_size.tar.zst"
 
     tar -cf - "$source_folder" | pv -pterb -s $(du -sb "$source_folder" | awk '{print $1}') | zstd -o "$target_file"
 done
