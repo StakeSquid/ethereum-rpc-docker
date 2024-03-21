a6ceb5c75866cd81f2bdcb1b0d5d348770b7f763
Sebastian <379651+czarly@users.noreply.github.com>
2024-03-21 09:09:28 +0100

    fiddle backups

--- backup-node.sh
+++ backup-node.sh
@@ -13,7 +13,19 @@ for key in $keys; do
     echo "Executing command with key: /var/lib/docker/volumes/rpc_$key/_data"
 
     source_folder="/var/lib/docker/volumes/rpc_$key/_data"
-    folder_size=$(du -sh "$source_folder" | awk '{print $1}')
+    folder_size=$(du -sh "$source_folder" | awk '{
+     size = $1
+     sub(/[Kk]$/, "", size)  # Remove 'K' suffix if present
+     sub(/[Mm]$/, "", size)  # Remove 'M' suffix if present
+     sub(/[Gg]$/, "", size)  # Remove 'G' suffix if present
+     if ($1 ~ /[Kk]$/) {
+         size *= 0.001   # Convert kilobytes to gigabytes
+     } else if ($1 ~ /[Mm]$/) {
+         size *= 0.001   # Convert megabytes to gigabytes
+     }
+     print size "G"
+    }')
+    
     target_file="/backup/rpc_$key-$(date +'%Y-%m-%d-%H-%M-%S')-$folder_size.tar.zst"
 
     tar -cf - "$source_folder" | pv -pterb -s $(du -sb "$source_folder" | awk '{print $1}') | zstd -o "$target_file"
