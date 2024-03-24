f8e1f0ccf445a43b246f77100f0f0cbcbf6b6f05
Sebastian <379651+czarly@users.noreply.github.com>
2024-03-25 00:10:51 +0100

    rreadable

--- endpoint-options.sh
+++ endpoint-options.sh
@@ -11,18 +11,14 @@ default_array=()
 for key in $(jq -r 'keys[]' <<< "$json"); do
     default_values=$(jq -r ".[\"$key\"].default[]" <<< "$json")
     default_array+=($default_values)
+
+    for node in "${default_values[@]}"; do
+      size_in_gb=$($BASEPATH/restore-volumes.sh "$node" --print-size-only)
+
+      if [ $? -eq 0 ]; then
+	echo "$key;$node;$size_in_gb"
+      fi
+    done
+    
 done
 
-for node in "${default_array[@]}"; do
-#echo "check $node"
-    size_in_gb=$($BASEPATH/restore-volumes.sh "$node" --print-size-only)
-
-    if [ $? -eq 0 ]; then
-	echo "$node: $size_in_gb"
-#    else
-#   	echo "$node can not"
-    fi
-done
-
-# Print the combined array
-#printf "%s\n" "${default_array[@]}"
