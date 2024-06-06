d7fbdfb28ecd4abb25f9ceb0b44574ae59e2333b
Sebastian <379651+czarly@users.noreply.github.com>
2024-06-06 08:31:38 +0200

    fix

--- taiko/script/start-driver.sh
+++ taiko/script/start-driver.sh
@@ -4,9 +4,9 @@ set -eou pipefail
 
 exec taiko-client driver \
      --l1.ws "${L1_ENDPOINT_WS}" \
-     --l2.ws ws://taiko:8545 \
+     --l2.ws "${L2_GETH_RPC:-ws://taiko:8545}" \
      --l1.beacon "${L1_BEACON_HTTP}" \
-     --l2.auth http://taiko:8551 \
+     --l2.auth "${L2_GETH_AUTH:-http://taiko:8551}" \
      --taikoL1 "${TAIKO_L1_ADDRESS}" \
      --taikoL2 "${TAIKO_L2_ADDRESS}" \
      --jwtSecret /data/taiko-geth/geth/jwtsecret \
