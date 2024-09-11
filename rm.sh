59b132ee62e56a559a52677eaed0333d783b2861
Sebastian <379651+czarly@users.noreply.github.com>
2024-09-11 15:00:23 +0200

    helpar

---
+++ rm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ ! -f "/root/rpc/$1.yml" ]; then
+    echo "Error: Either no argument provided or /root/rpc/$1.yml does not exist."
+    exit 1
+fi
+
+docker compose rm $(cat /root/rpc/$1.yml | yaml2json - | jq '.services' | jq -r 'keys[]' | tr '\n' ' ')
