7b0df6cbf87e58031650c0745d098c7587d415b4
Sebastian <379651+czarly@users.noreply.github.com>
2024-09-24 09:15:32 +0200

    update

--- network-to-config.sh
+++ network-to-config.sh
@@ -13,7 +13,7 @@ JSON_FILE="$BASEPATH/reference-rpc-endpoint.json"
 # Function to extract values for a given key
 extract_values() {
   local key=$1
-  cat "$JSON_FILE" | jq -r --arg key "$key" '.[$key] | .default? // .archive? // [] | .[]'
+  cat "$JSON_FILE" | jq -r --arg key "$key" '.[$key] | if .default then .default[] else [] end, if .archive then .archive[] else [] end'
 }
 
 # Initialize an empty result string
