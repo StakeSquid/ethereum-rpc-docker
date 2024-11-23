2c2a09878c8d79167d4b856c01788c543255e97a
Sebastian <379651+czarly@users.noreply.github.com>
2024-11-23 08:53:42 +0100

    update

---
+++ goat/testnet3/node-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ ! -d "/root/.ethereum/geth/chaindata" ]; then
+    geth init /genesis/geth.json
+else
+    echo "Chain already initialized."
+fi                                                                                                                                                                                                                   
+
+geth
+root@rpc-jp-1:~/rpc# cat goat/node-entrypoint.sh 
+#!/bin/sh
+
+# Custom initialization steps go here
+echo "Starting initialization steps..."
+
+# Check if the genesis.json file exists; if not, initialize and copy it
+if [ ! -f /goat/config/genesis.json ]; then
+    echo "Initializing goatd..."
+    goatd init --home /goat testnet
+    echo "Copying genesis file..."
+    cp /genesis/goat.json /goat/config/genesis.json
+else
+    echo "Genesis file already exists; skipping initialization."
+fi
+
+# Pass control to the final command specified in docker-compose.yml
+goatd start --home /goat --chain-id=goat-testnet3 --goat.geth /geth/geth.ipc --api.enable --api.address=tcp://0.0.0.0:1317 --p2p.persistent_peers c9befb6b8421b46ec5cda76171a8cf7539af20fc@52.12.249.134:26656 --p2p.external-address $IP:40258 --p2p.laddr 0.0.0.0:40258
