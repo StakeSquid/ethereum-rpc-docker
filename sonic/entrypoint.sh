925ccbadb927c430196d2caf031e773d7682b80b
Sebastian <379651+czarly@users.noreply.github.com>
2024-12-18 08:39:52 +0100

    first try

---
+++ sonic/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -f /var/sonic/initialized ]; then
+    echo "Initializing Sonic..."
+
+    # Add your initialization commands here
+    # Example:
+    # mkdir -p /var/sonic/data
+    # touch /var/sonic/config.json
+
+    wget https://genesis.soniclabs.com/sonic-mainnet/genesis/sonic.g.md5
+    GOMEMLIMIT=50GiB sonictool --datadir /var/sonic --cache 12000 genesis sonic.g.md5
+    rm sonic.g
+    
+    # Create the file to mark initialization
+    touch /var/sonic/initialized
+
+    echo "Initialization complete."
+else
+    echo "Sonic is already initialized."
+fi
+
+touch /var/sonic/initialized
+
+exec sonicd --datadir /var/sonic "$@"
