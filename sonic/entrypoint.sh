00dbee6696991f9e6e6d2bdab867dc9fb61b6548
Sebastian <379651+czarly@users.noreply.github.com>
2024-12-18 08:52:37 +0100

    fixes

--- sonic/entrypoint.sh
+++ sonic/entrypoint.sh
@@ -8,8 +8,8 @@ if [ ! -f /var/sonic/initialized ]; then
     # mkdir -p /var/sonic/data
     # touch /var/sonic/config.json
 
-    wget https://genesis.soniclabs.com/sonic-mainnet/genesis/sonic.g.md5
-    GOMEMLIMIT=50GiB sonictool --datadir /var/sonic --cache 12000 genesis sonic.g.md5
+    wget https://genesis.soniclabs.com/sonic-mainnet/genesis/sonic.g
+    GOMEMLIMIT=50GiB sonictool --datadir /var/sonic --cache 12000 genesis sonic.g
     rm sonic.g
     
     # Create the file to mark initialization
