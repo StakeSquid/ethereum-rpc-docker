3ad79dced3efd8bf428c88db6108ec45a874e4cb
Sebastian <379651+czarly@users.noreply.github.com>
2024-12-22 08:26:05 +0100

    executable

---
+++ goat/mainnet/geth-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ ! -d "/root/.ethereum/geth/chaindata" ]; then
+    geth init /genesis/geth.json
+else
+    echo "Chain already initialized."
+fi
+
+geth --goat=mainnet
---
+++ goat/mainnet/node-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Custom initialization steps go here
+echo "Starting initialization steps..."
+
+# Check if the genesis.json file exists; if not, initialize and copy it
+if [ ! -f /goat/config/genesis.json ]; then
+    echo "Initializing goatd..."
+    goatd init --home /goat mainnet
+    echo "Copying genesis file..."
+    cp /genesis/goat.json /goat/config/genesis.json
+else
+    echo "Genesis file already exists; skipping initialization."
+fi
+
+# Pass control to the final command specified in docker-compose.yml
+goatd start --home /goat --chain-id=goat-mainnet --goat.geth /geth/geth.ipc --api.enable --api.address=tcp://0.0.0.0:1317 --p2p.external-address $IP:40258 --p2p.laddr 0.0.0.0:40258
