ba36be78d8e4ebbc56bfdf1aac09b67c9ac77ac6
Sebastian <379651+czarly@users.noreply.github.com>
2025-01-05 05:48:38 +0100

    fix

--- backup-node.sh
+++ backup-node.sh
@@ -40,8 +40,8 @@ for key in $keys; do
     #echo "$target_file"
 
     if [[ -n $2 ]]; then
-	tar -cf - "$source_folder" | pv -pterb -s $(du -sb "$source_folder" | awk '{print $1}') | zstd | curl -X PUT --upload-file - "$2/uploading-$target_file"
-        curl -X MOVE -H "Destination: $2/$target_file" "$2/uploading-$target_file"
+	tar -cf - "$source_folder" | pv -pterb -s $(du -sb "$source_folder" | awk '{print $1}') | zstd | curl -X PUT --upload-file - "$2/null/uploading-$target_file"
+        curl -X MOVE -H "Destination: /null/$target_file" "$2/uploading-$target_file"
     else    
         tar -cf - "$source_folder" | pv -pterb -s $(du -sb "$source_folder" | awk '{print $1}') | zstd -o "/backup/uploading-$target_file"
         mv "/backup/uploading-$target_file" "/backup/$target_file"
