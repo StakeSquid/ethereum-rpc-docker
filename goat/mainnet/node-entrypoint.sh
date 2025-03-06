487aca28515b7b075dc11de1f919c62f77c3c77e
Sebastian <379651+czarly@users.noreply.github.com>
2025-03-06 05:11:43 +0100

    make sure the process runs as pid 0 to allow docker to stop the containdr

--- goat/mainnet/node-entrypoint.sh
+++ goat/mainnet/node-entrypoint.sh
@@ -6,7 +6,7 @@ echo "Starting initialization steps..."
 # Check if the genesis.json file exists; if not, initialize and copy it
 if [ ! -f /goat/config/genesis.json ]; then
     echo "Initializing goatd..."
-    goatd init --home /goat mainnet
+    exec goatd init --home /goat mainnet
     echo "Copying genesis file..."
     cp /genesis/goat.json /goat/config/genesis.json
 else
@@ -14,4 +14,4 @@ else
 fi
 
 # Pass control to the final command specified in docker-compose.yml
-goatd start --home /goat --chain-id=goat-mainnet --goat.geth /geth/geth.ipc --api.enable --api.address=tcp://0.0.0.0:1317 --p2p.external-address $IP:40258 --p2p.laddr 0.0.0.0:40258
+exec goatd start --home /goat --chain-id=goat-mainnet --goat.geth /geth/geth.ipc --api.enable --api.address=tcp://0.0.0.0:1317 --p2p.external-address $IP:40258 --p2p.laddr 0.0.0.0:40258
