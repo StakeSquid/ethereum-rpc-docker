f708f65cfcf3a5a360630ad924be9fdbe16d9361
Sebastian <379651+czarly@users.noreply.github.com>
2025-03-06 05:06:26 +0100

    make sure the process id is 0 by using exec

--- goat/mainnet/geth-entrypoint.sh
+++ goat/mainnet/geth-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ ! -d "/root/.ethereum/geth/chaindata" ]; then
-    geth init /genesis/geth.json
+    exec geth init /genesis/geth.json
 else
     echo "Chain already initialized."
 fi
 
-geth --goat=mainnet
+exec geth --goat=mainnet
