97f0148e427ce62b59621c9f7e1f2c3a6ac778c1
Sebastian <379651+czarly@users.noreply.github.com>
2025-03-18 13:10:35 +0100

    timeout everytwhere

--- check-health.sh
+++ check-health.sh
@@ -70,7 +70,7 @@ if [ $? -eq 0 ]; then
                         exit 1
                     fi
 		else 
-		    "unverified ($http_status_code2)"
+		    echo "unverified ($http_status_code2)"
 		    exit 0
                 fi 
             fi
