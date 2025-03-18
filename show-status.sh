9a07aafabb919dc1a94af7b55b32cb24457622e4
Sebastian <379651+czarly@users.noreply.github.com>
2025-03-18 12:15:07 +0100

    fix the monitoring

--- show-status.sh
+++ show-status.sh
@@ -37,10 +37,10 @@ for part in "${parts[@]}"; do
 
 	if [[ "$result" == *"syncing"* && $? -eq 1 ]]; then
 	    # Allow exit status 1 if result contains "syncing"
-	    true
+	    echo "hit syncing"
 	elif [[ "$result" == *"lagging"* && $? -eq 1 ]]; then
 	    # Allow exit status 1 if result contains "syncing"
-	    true
+	    echo "hit lagging"
 	elif [ $? -ne 0 ]; then
 	    any_failure=true
 	fi
