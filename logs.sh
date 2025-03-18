dadca852fd19e0f2ada73b2bb58aa20e01837adc
Sebastian <379651+czarly@users.noreply.github.com>
2025-03-18 10:24:38 +0100

    only the last hours

--- logs.sh
+++ logs.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ] || [ ! -f "/root/rpc/$1.yml" ]; then
     exit 1
 fi
 
-docker compose logs -f $(cat /root/rpc/$1.yml | yaml2json - | jq '.services' | jq -r 'keys[]' | tr '\n' ' ')
+docker compose logs -f --since "2h" $(cat /root/rpc/$1.yml | yaml2json - | jq '.services' | jq -r 'keys[]' | tr '\n' ' ')
