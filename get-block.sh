57eecf611fe2980913c278072ea23dd2e00a1441
squidbear <379651+czarly@users.noreply.github.com>
2025-03-24 05:04:22 +0100

    fix

--- get-block.sh
+++ get-block.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-request="{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"${2:-latest}\",false],\"id\":1}' | jq -r '.result.number, .result.hash' | gawk '{if (NR==1) print \"Block Number:\", strtonum($0); else print \"Block Hash:\", $0}"
+request="{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"${2:-latest}\",false],\"id\":1}" 
 
 echo "${request}"
 
-curl -s -X POST "${1}" -H "Content-Type: application/json" --data "'${request}'"
+curl -s -X POST "${1}" -H "Content-Type: application/json" --data "'${request}'" | jq -r '.result.number, .result.hash' | gawk '{if (NR==1) print "Block Number:", strtonum($0); else print "Block Hash:", $0}'
