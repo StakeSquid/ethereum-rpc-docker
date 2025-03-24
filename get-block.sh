9049a88672dbb083cafa03543ea2a0f1a7a15f4f
squidbear <379651+czarly@users.noreply.github.com>
2025-03-24 05:00:15 +0100

    convenient

---
+++ get-block.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl -s -X POST "${1}" -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"${2:-latest}\",false],\"id\":1}' | jq -r '.result.number, .result.hash' | gawk '{if (NR==1) print \"Block Number:\", strtonum($0); else print \"Block Hash:\", $0}"
