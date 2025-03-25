1056feab7ec4c763b7dc5c951302b85bd7144cb4
squidbear <379651+czarly@users.noreply.github.com>
2025-03-25 09:22:58 +0100

    do fantom with sonic as well

---
+++ fantom/scripts/entrypoint.sonic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+datadir=/var/sonic
+
+if [ ! -f "$datadir/initialized" ]; then
+    echo "Initializing Sonic..."
+
+    url="${GENESIS:-https://download.fantom.network/opera/mainnet/mainnet-5577-archive.g}"
+    filename=$(basename "$url")
+    
+    wget -P "$datadir" "$url"
+
+    GOMEMLIMIT="${CACHE_GB}GiB" sonictool --datadir "$datadir" --cache "${CACHE_GB}000" genesis "$datadir/$filename"
+    rm "$datadir/$filename"
+    
+    touch "$datadir/initialized"
+
+    echo "Initialization complete."
+else
+    echo "Sonic is already initialized."
+fi
+
+exec sonicd --cache "${CACHE_GB}000" --datadir "$datadir" "$@"
