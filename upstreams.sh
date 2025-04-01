eeb94ff99a539597209abf9a9b3aa6438091cc12
squidbear <379651+czarly@users.noreply.github.com>
2025-04-01 07:29:38 +0200

    fix

--- upstreams.sh
+++ upstreams.sh
@@ -93,9 +93,9 @@ for part in "${parts[@]}"; do
 		[ -f "$input_file" ] || input_file="$BASEPATH/default.cfg"
 
 		# Run envsubst to replace environment variables in the input file and save the result to the output file
-		if yaml2json < tron-mainnet.yml | jq -e '.upstreams' >/dev/null 2>&1; then
+		if yaml2json < "$BASEPATH/$part" | jq -e '.upstreams' >/dev/null 2>&1; then
 		    echo "upstreams key exists in $part"
-		    upstreams+=($(yaml2json < tron-mainnet.yml | jq '.upstreams' | sed 's/^/  /' | envsubst | json2yaml))
+		    upstreams+=($(yaml2json < "$BASEPATH/$part" | jq '.upstreams' | sed 's/^/  /' | envsubst | json2yaml))
 		else
 		    echo "upstreams config $input_file for $part"
 		    upstreams+=("$(envsubst < "$input_file")")		    
