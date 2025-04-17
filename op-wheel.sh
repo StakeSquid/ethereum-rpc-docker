3167e1e80d3137452756e4cd13321b933c68d799
Para Dox <armhole.limeade_0l@icloud.com>
2025-04-17 15:40:28 +0700

    fix

--- op-wheel.sh
+++ op-wheel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # USAGE:
-# ./op-wheel.sh engine set-forkchoice --unsafe=0x111AC7F --safe=0x111AC7F --finalized=0x111AC7F --engine=http://op-lisk-sepolia:8551/ --engine.open=http://op-lisk-sepolia:8545 --engine.jwt-secret-path=/jwtsecret
+#  docker stop rpc-celo-alfajores-node-1 && ./op-wheel.sh engine set-forkchoice --unsafe=43897488 --safe=43897488 --finalized=43897488 --engine=http://celo-alfajores:8551 --engine.jwt-secret-path=/jwtsecret --engine.open=http://celo-alfajores:8545
 
 docker run -it --rm --network rpc_chains -v /root/rpc/.jwtsecret:/jwtsecret golang:latest bash -c 'git clone https://github.com/ethereum-optimism/optimism.git && cd optimism && go run ./op-wheel/cmd "$@"' -- "$@"
 
