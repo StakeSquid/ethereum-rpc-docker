e7657a133cc2fa98a9ed33ba062e5434bd3e99bf
Para Dox <armhole.limeade_0l@icloud.com>
2025-04-27 22:48:49 +0700

    simple look

--- logging-proxy/proxy.py
+++ logging-proxy/proxy.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import requests
 import sys
 import os
+import json
 
 app = Flask(__name__)
 
@@ -11,12 +12,19 @@ TARGET_URL = os.getenv('TARGET_URL', 'http://host.docker.internal:8545')
 @app.route('/', methods=['POST'])
 def proxy():
     incoming = request.get_json()
-    print(f"==> Request:\n{incoming}", file=sys.stdout, flush=True)
+    request_log = f"==> Request:\n{json.dumps(incoming, indent=2)}"
 
     response = requests.post(TARGET_URL, json=incoming)
     outgoing = response.json()
 
-    print(f"<== Response:\n{outgoing}", file=sys.stdout, flush=True)
+    log_lines = [request_log]
+
+    if 'error' in outgoing:
+        response_log = f"<== Response (Error):\n{json.dumps(outgoing, indent=2)}"
+        log_lines.append(response_log)
+
+    print('\n---\n'.join(log_lines), file=sys.stdout, flush=True)
+        
     return jsonify(outgoing)
 
 if __name__ == '__main__':
