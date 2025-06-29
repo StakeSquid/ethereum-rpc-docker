6abf4b2a0052d4ebd5d2222ce5eb682163dab28c
goldsquid <goldberg@stakesquid.com>
2025-06-29 14:26:53 +0700

    more stats

--- show-cpu.sh
+++ show-cpu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
- awk '/MHz/ {sum+=$4; count++} END {print sum/count " MHz"}' /proc/cpuinfo
+# awk '/MHz/ {sum+=$4; count++} END {print sum/count " MHz"}' /proc/cpuinfo
+awk '/MHz/ {freq[NR]=$4; sum+=$4; count++} END {asort(freq); print "Min:", freq[1], "MHz\nMax:", freq[count], "MHz\nAvg:", sum/count, "MHz"}' /proc/cpuinfo
